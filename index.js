ca9f7cedb20c79ab7162fd54f5d57ebf71b42fc6
Rinat Sadykov <sadykovra@mail.ru>
2018-09-26 16:03:22 +0600

    Did the task#3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const VERSION = `0.0.1`;
+const AUTHOR = `Rinat Sadykov`;
+const PROJECT_NAME = `nodejs-intensive`;
 
 const command = process.argv[2];
 
@@ -22,10 +24,11 @@ if (isAvaliableCommands) {
   commandActions[command]();
   process.exit(0);
 } else if (!command) {
-  console.log(`Привет пользователь!`);
+  console.log(`Привет пользователь!Эта программа будет запускать сервер ${PROJECT_NAME}.
+    Автор: ${AUTHOR}.`);
   process.exit(0);
 } else {
-  console.error(`Неизвестная команда ${command}`);
+  console.error(`Неизвестная команда ${command}.Чтобы прочитать правила использования приложения, наберите "--help"`);
   process.exit(1);
 }
 
