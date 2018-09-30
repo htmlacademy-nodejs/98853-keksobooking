3c02612e3fd7ec862296c009cb7440d1e2ecbaef
Rinat Sadykov <sadykovra@mail.ru>
2018-09-30 21:01:10 +0500

    Fix task#5 Remove isCommandAvaliable

--- src/init.js
+++ src/init.js
@@ -10,16 +10,17 @@ const license = require(`./commands/license.js`);
 
 const commands = [help, version, author, description, license];
 const init = (command) => {
-  const isCommandAvailable = commands.some((elem) => `--${elem.name}` === command);
-  if (isCommandAvailable) {
-    commands.find((elem) => `--${elem.name}` === command).execute();
-  } else if (!command) {
+  if (command) {
+    const match = commands.find((elem) => `--${elem.name}` === command);
+    if (match) {
+      match.execute();
+    } else {
+      console.log(`Неизвестная команда ${command}.Чтобы прочитать правила использования приложения, наберите "--help"`);
+      process.exit(1);
+    }
+  } else {
     console.log(`Привет пользователь! Эта программа будет запускать сервер ${packageInfo.name}.
 	  	\nАвтор: ${packageInfo.author}.`);
-  } else {
-    console.log(`Неизвестная команда ${command}.
-    	\nЧтобы прочитать правила использования приложения, наберите "--help"`);
-    process.exit(1);
   }
 };
 
