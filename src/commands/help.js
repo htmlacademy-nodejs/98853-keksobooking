1d0b7eca0a2a39b175f801b5a34668c8389dbb1c
Rinat Sadykov <sadykovra@mail.ru>
2018-10-01 15:02:21 +0500

    Did the task#6

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,6 +4,7 @@ const version = require(`./version.js`);
 const author = require(`./author.js`);
 const description = require(`./description.js`);
 const license = require(`./license.js`);
+require(`colors`);
 
 const commands = [version, author, description, license];
 
@@ -11,9 +12,9 @@ module.exports = {
   name: `help`,
   description: `Показывает список доступных команд`,
   execute() {
-    console.log(`${this.name} - ${this.description}`);
+    console.log(`--${this.name.grey} - ${this.description.green}`);
     commands.forEach((elem) => {
-      console.log(`${elem.name} - ${elem.description}`);
+      console.log(`--${elem.name.grey} - ${elem.description.green}`);
     });
   }
 };
--- src/commands/version.js
+++ src/commands/version.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const {version} = require(`../../package.json`);
+require(`colors`);
 
 module.exports = {
   name: `version`,
   description: `Печатает версию приложения`,
   execute() {
-    console.log(`v${version}`);
+    console.log(`v${version[0].red}.${version[2].green}.${version[4].blue}`);
   }
 };
