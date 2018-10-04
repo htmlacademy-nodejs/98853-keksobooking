5a7ccea80c35b236c38a9119748b1b7a054e783c
Rinat Sadykov <sadykovra@mail.ru>
2018-10-04 21:39:04 +0500

    Did the task #8

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 require(`colors`);
 const init = require(`./src/init.js`);
-const {generateEntity} = require(`./src/generate.js`);
-
-console.log(generateEntity());
 const command = process.argv[2];
 
 init(command);
